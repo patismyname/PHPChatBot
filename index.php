86d8186a9042f7bf9f34bb5bc3422d83cb4fb8e9
patismyname <patismyname@gmail.com>
2017-06-02 00:01:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,3 +46,32 @@ $result = curl_exec($ch);
 curl_close ($ch);
  
 ?>
+<?php
+ 
+$strAccessToken = "IrePbVqF/xw6DzbPf5e4Q2hoCNBfuV8D5mdex4PzpFGSb9BBkiCYmm88aROLyAdZ/Lrg/9AMgSQjTQi639vo2ZDhdSOYzV/u1GEenP8SNBYP5d+AZIJF0cV2Qw0Ng7ABVLRFhdwmbz25jaUSPhKXpAdB04t89/1O/w1cDnyilFU=";
+ 
+ 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = "U55ce393057424275d1ef50944162967d";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
